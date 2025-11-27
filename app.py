48f49d998bfb1ac006fc2b4809af511a098d0f48
Jaynish Shah <10665032+jsha129@users.noreply.github.com>
2025-11-27 11:27:31 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
+# ---- File: app.py ----
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Uploader", layout="wide")
 
-st.title("📁 Upload Excel File")
+@st.cache_data
+def load_data(uploaded_file):
+    df = pd.read_excel(uploaded_file,sheet_name = "Sheet1", nrows = 1000)
+    df = df.rename(columns={'DE_in_some': 'DE_in_some_cells', 'is_DE_BS_EC': 'DE_in_Bstem_Endothelial'})
+    df = df.sort_values(by = 'gene').reset_index()
+    df = df.drop(columns = 'index')
+    return df
 
-uploaded = st.file_uploader("Choose Excel file", type=["xlsx", "xls"])
 
-if uploaded:
-    df = pd.read_excel(uploaded, sheet_name = "Sheet1")
-    df = df.sort_values(by = 'Gene')
-    st.session_state["df"] = df
-    st.success("File loaded successfully!")
+st.set_page_config(page_title="Merscope gene selection app")
 
-    # Move to next page
-    st.page_link("pages/2_Form_Page.py", label="Go to Form Page →")
+
+st.title("Upload File")
+uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
+
+
+if uploaded_file:
+    st.session_state.df = load_data(uploaded_file)
+    st.session_state.idx = 0
+    st.switch_page("pages/record_view.py")
