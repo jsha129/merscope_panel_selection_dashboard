aa6101e35351d120eee4bf143dd7f209c6b92b2c
Jaynish Shah <10665032+jsha129@users.noreply.github.com>
2025-12-04 10:58:39 +1100

    Update db_test1.py

--- pages/db_test1.py
+++ pages/db_test1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg2://neondb_owner:npg_2zsJAF3pECPo@ep-icy-dawn-a7q2fmwh-pooler.ap-southeast-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+engine = create_engine("postgresql://neondb_owner:npg_2zsJAF3pECPo@ep-icy-dawn-a7q2fmwh-pooler.ap-southeast-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
 df = pd.read_sql("SELECT * FROM home LIMIT 10;", engine)
 
